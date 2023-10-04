719eea4451daf2316ac5e8bf692da736d985ced6
collinear <118291046+collinearen@users.noreply.github.com>
2023-10-04 23:18:35 +0300

    in the first commit I added a file with a connection to the database

---
+++ src/create_new_db.py
@@ -0,0 +1,40 @@
+import sqlite3
+from sqlite3 import Error
+
+
+def create_connection(db_file):
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print(e)
+
+    return conn
+
+
+def create_table(conn, create_table_sql):
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
+
+
+def main():
+    database = r"dictionary_my.db"
+    sql_create_dictionary_table = """ CREATE TABLE IF NOT EXISTS dictionary (
+                                        id integer PRIMARY KEY,
+                                        word text,
+                                        meaning text
+                                    ); """
+
+    conn = create_connection(database)
+    if conn is not None:
+        create_table(conn, sql_create_dictionary_table)
+    else:
+        print("Ошибка: не удалось подключиться к базе.")
+
+
+if __name__ == '__main__':
+    main()
